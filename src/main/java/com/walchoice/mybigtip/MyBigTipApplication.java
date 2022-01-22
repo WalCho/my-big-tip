0633f3e85e3b6decd69940b71cefdb627eed4cac
MacChoice <nikos.valtsiogis@gmail.com>
2022-01-22 22:25:44 +0200

    User Verification: Created endpoint to verify users, send emails asynchronously.

--- src/main/java/com/walchoice/mybigtip/MyBigTipApplication.java
+++ src/main/java/com/walchoice/mybigtip/MyBigTipApplication.java
@@ -2,8 +2,10 @@ package com.walchoice.mybigtip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MyBigTipApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/walchoice/mybigtip/service/AuthService.java
+++ src/main/java/com/walchoice/mybigtip/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.walchoice.mybigtip.service;
 
 import com.walchoice.mybigtip.dto.RegisterRequest;
+import com.walchoice.mybigtip.exceptions.SpringRedditException;
 import com.walchoice.mybigtip.model.NotificationEmail;
 import com.walchoice.mybigtip.model.User;
 import com.walchoice.mybigtip.model.VerificationToken;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -49,5 +51,20 @@ public class AuthService {
 
         verificationTokenRepository.save(verificationToken);
         return token;
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        verificationToken.orElseThrow(() -> new SpringRedditException("Invalid Token"));
+        fetchUserAndEnable(verificationToken.get());
+    }
+
+    @Transactional
+    public void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new SpringRedditException("User not found with name - " + username));
+        user.setEnabled(true);
+        userRepository.save(user);
     }
 }
